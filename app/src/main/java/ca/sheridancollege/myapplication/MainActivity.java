6cbad254df18100a317836475ae6e40ecef1f3a0
Ellison Matienzo <matienzo@shernet.sheridancollege.ca>
2018-12-12 13:06:43 -0500

    Darkness

--- app/src/main/java/ca/sheridancollege/myapplication/MainActivity.java
+++ app/src/main/java/ca/sheridancollege/myapplication/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.DarkTheme);
         setContentView(R.layout.activity_main);
 
         mRecyclerView = findViewById(R.id.recycler_view);
